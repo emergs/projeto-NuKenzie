e8ff7bc6235af6d0bc31a7eb9845c8e928e90dec
Emerson Gonçalves dos Santos <emer_gs@hotmail.com>
2023-03-14 04:49:51 +0000

    feat:validação do formulário

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,12 +1,17 @@
+import { ErrorMessage } from '@hookform/error-message';
+import { click } from '@testing-library/user-event/dist/click';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './style.css';
 
-const Form = ({listTransactions, setListTransactions, filterListTransactions, setFilterListTransactions})=>{
-  const[description,setDescription] = useState('');
-  const[value,setValue] = useState();
-  const[typeInput,setTypeInput] = useState('');
-  
-  const addItemList = (event,desc,value,input) =>{
+const Form = ({ listTransactions, setListTransactions, filterListTransactions, setFilterListTransactions }) => {
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState();
+  const [typeInput, setTypeInput] = useState('');
+
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const addItemList = (event, desc, value, input) => {
     event.preventDefault()
     setDescription('');
     setValue('');
@@ -14,36 +19,52 @@ const Form = ({listTransactions, setListTransactions, filterListTransactions, se
     const newItem = {
       description: desc,
       type: input,
-      value: input === 'entrada'? parseInt(value) : parseInt(-value)
+      value: input === 'entrada' ? parseInt(value) : parseInt(-value)
     }
-    
-    setListTransactions([...listTransactions,newItem]);
-    setFilterListTransactions([...filterListTransactions,newItem]);
+
+
+
+    setListTransactions([...listTransactions, newItem]);
+    setFilterListTransactions([...filterListTransactions, newItem]);
   }
 
-  return(
-    <form action="" className="formMain">
+  const handleClick = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <form className="formMain" onSubmit={handleSubmit(handleClick)}>
+
       <div className="formDescription">
-        <label htmlFor="">Descrição</label>
-        <input required type="text" placeholder="Digite aqui sua descrição" value={description} onChange={(e)=> setDescription(e.target.value)} />
+        <label htmlFor="desc">Descrição</label>
+        <input {...register("description", { required: true })} />
+        {errors.description && <p className='error-message'>Preenchimento obrigatório</p>}
         <span>Ex: Compra de Roupas</span>
       </div>
+
       <div className="formValueAndType">
         <div>
           <label htmlFor="">Valor</label>
-          <input type="number" value={value} onChange={(e)=> setValue(e.target.value)} required/> 
+          <input name='valor' {...register("value", {
+            required: true,
+            pattern: /^[0-9]+$/
+          })} />
+          {errors.value?.type === 'required' && <p className='error-message'>Preenchimento obrigatório</p>}
+          {errors.value?.type === 'pattern' && <p className='error-message'>Digite somente números</p>}
         </div>
+
         <div>
           <label htmlFor="">Tipo de valor</label>
-          <select name="" id="select_options" value={typeInput} onChange={(e)=> setTypeInput(e.target.value)} required>
-            <option value=""></option>
+          <select id="select_options" placeholder='selecione'{...register('select_options')}>
             <option value="entrada">Entrada</option>
             <option value="despesa">Despesa</option>
           </select>
         </div>
+
       </div>
+
       <div className="formButton">
-        <button type='button' onClick={(event) => addItemList(event,description,value,typeInput)}>Inserir valor</button>
+        <button type='submit'>Inserir valor</button>
       </div>
     </form>
   )
